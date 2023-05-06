9f8afa7cf3b9fdc9da60f2c51e1b3b1ca5f47be8
KaterinaKha <119073906+KaterinaKha@users.noreply.github.com>
2023-05-06 22:12:16 +0000

    Deploying to gh-pages from @ KaterinaKha/goit-js-hw-09@b2a003356e016b8c422de1adc21ce18c86556efb 🚀

--- 01-color-switcher.6f8eb490.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let o=null;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,console.log(document.body.style.backgroundColor)}t.startBtn.addEventListener("click",(function(){o=setInterval(n,1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearTimeout(o),t.startBtn.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.6f8eb490.js.map
