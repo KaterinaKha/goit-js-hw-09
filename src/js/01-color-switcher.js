4645c84eefb9d8e66c6f3797cc4678f20234a3cd
KaterinaKha <katerinakhaa@gmail.com>
2023-05-07 00:00:49 +0200

    Create repo ones more

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', changeBackgroundColors);
+refs.stopBtn.addEventListener('click', stopChangeBackgroundColors);
+
+function changeBackgroundColors() {
+  intervalId = setInterval(attachBackgroundColor, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function stopChangeBackgroundColors() {
+  clearTimeout(intervalId);
+  refs.startBtn.disabled = false;
+  // refs.stopBtn.disabled = true;
+}
+
+function attachBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  console.log(document.body.style.backgroundColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
